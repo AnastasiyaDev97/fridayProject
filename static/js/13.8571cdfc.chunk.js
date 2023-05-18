7792e7d56339376c8b1753fe4a8a4dea4c7e8c00
NastyaZ23 <nastyh1233@gmail.com>
2023-05-18 13:11:23 +0300

    Updates

--- static/js/13.8571cdfc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[13],{288:function(t,e,c){"use strict";c.r(e);var p=c(0),s=c(75),n=c(1),a=Object(p.memo)((function(){return Object(n.jsx)("div",{children:Object(n.jsx)(s.a,{})})}));e.default=a}}]);
-//# sourceMappingURL=13.8571cdfc.chunk.js.map
